3ca8a0515cf5ecc798d0e940d31dfd942f1bb7ec
Andrew Burkholder <hi@andrew.vip>
2018-06-25 22:10:13 -0400

    add additional heading and make second heading change via use of id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
  const button = document.querySelector('button');
  button.addEventListener('click', updateHeading);
- const heading = document.querySelector('h1');
+ const heading = document.querySelector('#changeThis');
 
  function updateHeading() {
     heading.innerHTML = 'Too many chrisses!'

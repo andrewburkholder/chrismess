33f672afa38c7d9b7509feca203b194701a46c91
Andrew Burkholder <hi@andrew.vip>
2018-06-25 22:06:02 -0400

    make button change heading text

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('It works!')
+ const button = document.querySelector('button');
+ button.addEventListener('click', updateHeading);
+ const heading = document.querySelector('h1');
+
+ function updateHeading() {
+    heading.innerHTML = 'Too many chrisses!'
+ }

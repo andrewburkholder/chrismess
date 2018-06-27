5247fb18e464c59599bc77224599f5cd9c26b9f9
Andrew Burkholder <hi@andrew.vip>
2018-06-27 14:36:02 -0400

    break creating elements into separate function

--- index.js
+++ index.js
@@ -4,22 +4,25 @@ document.querySelector('form').addEventListener("submit", addToList);
 function addToList(ev) {
     ev.preventDefault();  //prevents page from reloading on submit
     const f = ev.target;
-    
-    const flickName = f.flickName.value;
-    const flickYear = f.flickYear.value;
+
+    const nameSpan = renderProperty("name", f.flickName.value);
+    const yearSpan = renderProperty("year", f.flickYear.value);
+
 
     const list = document.querySelector("#flicks");
     const newLi = document.createElement('li');
-    const flickNameSpan = document.createElement("span");
-    const flickYearSpan = document.createElement("span");
-    flickNameSpan.textContent = flickName;
-    flickNameSpan.classList.add("flickName");
-    flickYearSpan.textContent = flickYear;
-    flickYearSpan.classList.add("flickYear");
-    newLi.appendChild(flickNameSpan);
-    newLi.appendChild(flickYearSpan);
+    
+    newLi.appendChild(nameSpan);
+    newLi.appendChild(yearSpan);
     newLi.classList.add("flick");
     list.appendChild(newLi);
 
     f.reset();
+}
+
+function renderProperty(name, value) {
+    const span = document.createElement("span");
+    span.classList.add(name);
+    span.textContent = value;
+    return span;
 }

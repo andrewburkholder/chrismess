4aa4f291be7b216d2b209da7ff2583ae30280eb1
Andrew Burkholder <hi@andrew.vip>
2018-06-27 22:15:26 -0400

    copy Davey's HTML & JS and change css to update branch with inclass work

--- index.js
+++ index.js
@@ -1,28 +1,52 @@
-const heading = document.querySelector("#changeThis");
-document.querySelector('form').addEventListener("submit", addToList);
-
-function addToList(ev) {
-    ev.preventDefault();  //prevents page from reloading on submit
-    const f = ev.target;
-
-    const nameSpan = renderProperty("name", f.flickName.value);
-    const yearSpan = renderProperty("year", f.flickYear.value);
-
-
-    const list = document.querySelector("#flicks");
-    const newLi = document.createElement('li');
-    
-    newLi.appendChild(nameSpan);
-    newLi.appendChild(yearSpan);
-    newLi.classList.add("flick");
-    list.appendChild(newLi);
-
-    f.reset();
-}
-
-function renderProperty(name, value) {
-    const span = document.createElement("span");
-    span.classList.add(name);
-    span.textContent = value;
-    return span;
-}
+class App {
+    constructor() {
+      const form = document.querySelector('form#flickForm')
+      form.addEventListener('submit', (ev) => {
+        ev.preventDefault()
+        this.handleSubmit(ev)
+      })
+    }
+  
+    renderProperty(name, value) {
+      const span = document.createElement('span')
+      span.classList.add(name)
+      span.textContent = value
+      return span
+    }
+  
+    renderItem(flick) {
+      const item = document.createElement('li')
+      item.classList.add('flick')
+  
+      // get the list of properties
+      const properties = Object.keys(flick)
+  
+      // loop over the properties
+      properties.forEach((propertyName) => {
+        // build a span, and append it to the list
+        const span = this.renderProperty(propertyName, flick[propertyName])
+        item.appendChild(span)
+      })
+  
+      return item
+    }
+  
+    handleSubmit(ev) {
+      const f = ev.target
+  
+      const flick = {
+        name: f.flickName.value,
+        chris: f.chrisName.value,
+      }
+  
+      const item = this.renderItem(flick)
+  
+      const list = document.querySelector('#flicks')
+      list.appendChild(item)
+  
+      f.reset()
+      f.flickName.focus()
+    }
+  }
+  
+  const app = new App()
